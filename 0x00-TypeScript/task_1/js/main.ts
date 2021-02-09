7529f824264b14f553bc54b2ce4f40c7f9501504
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-09 20:34:52 +0200

    Complete task-3

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -10,3 +10,12 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+interface printTeacherFunction {}
+
+let printTeacher: printTeacherFunction;
+
+printTeacher = (firstName: string, lastName: string) => {
+  let fl = firstName.slice(0, 1);
+  return `${fl}. ${lastName}`;
+};
