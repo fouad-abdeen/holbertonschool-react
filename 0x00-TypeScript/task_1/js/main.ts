bf0eea9aaaef1859c7db1170ea233fefae960be6
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-09 21:15:19 +0200

    Complete task-4

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -21,3 +21,25 @@ printTeacher = (firstName, lastName) => {
   let fl = firstName.slice(0, 1);
   return `${fl}. ${lastName}`;
 };
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): IStudent;
+}
+
+interface IStudent {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass
+  implements IStudent {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework() {
+    return "Currently working";
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+};
