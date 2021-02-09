5da790273ef6dcdaeabf1c3d6095e5aef2ea12b6
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-09 20:08:42 +0200

    Complete task-1

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -7,12 +7,4 @@ interface Teacher {
   [key: string]: any;
 }
 
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-
-console.log(teacher3);
+export default Teacher;
