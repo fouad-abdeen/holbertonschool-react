b1bc8ebae3a1bfe5a207e1eede47e7e30ec7d079
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-11 16:33:16 +0200

    Complete task-2

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,28 @@
+import '../css/main.css';
+import $ from 'jquery';
+
+let p1= document.createElement('p');
+let p2= document.createElement('p');
+let btn = document.createElement('button');
+let p3 =  document.createElement('p');
+let p4 = document.createElement('p');
+
+p1.innerHTML = 'Holberton Dashboard';
+p2.innerHTML = 'Dashboard data for the students';
+btn.innerHTML = 'Click here to get started';
+p3.setAttribute('id','count');
+p4.innerHTML = 'Copyright - Holberton School';
+
+document.addEventListener('DOMContentLoaded', () => {
+  $("body").append(p1);
+  $("body").append(p2);
+  $("body").append(btn);
+  $("body").append(p3);
+  $("body").append(p4);
+});
+
+let count = 0;
+btn.addEventListener('click', () => {
+  count += 1;
+  p3.innerHTML = `${count} clicks on the button`;
+});
