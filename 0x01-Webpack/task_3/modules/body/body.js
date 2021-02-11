0d3dedbc68a34d4f2419241e2f94463674bce30a
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-11 21:28:07 +0200

    Complete task-3

--- 0x01-Webpack/task_3/modules/body/body.js
+++ 0x01-Webpack/task_3/modules/body/body.js
@@ -2,19 +2,28 @@ import './body.css';
 import $ from 'jquery';
 import _ from 'lodash';
 
+
+let par= document.createElement('p');
 let btn = document.createElement('button');
 let ctn =  document.createElement('p');
 
+par.innerHTML = 'Dashboard data for the students';
 btn.innerHTML = 'Click here to get started';
 ctn.setAttribute('id','count');
 
 document.addEventListener('DOMContentLoaded', () => {
+  $("body").append(par);
   $('body').append(btn);
   $('body').append(ctn);
 });
 
 let count = 0;
-btn.addEventListener('click', () => {
+
+const counter = () => {
   count += 1;
   ctn.innerHTML = `${count} clicks on the button`;
-});
+}
+
+btn.addEventListener('click', counter);
+
+$('button').on('click', _.debounce(counter));
