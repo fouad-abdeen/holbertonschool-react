6a871cf2e8e66c5f165e2420818b95d61a4fd092
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-18 21:15:15 +0200

    Modify package.json/package-lock.json and Implement testing for App/Notifications components

--- 0x02-react_intro/task_3/dashboard/src/App.test.js
+++ 0x02-react_intro/task_3/dashboard/src/App.test.js
@@ -2,8 +2,25 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 
-describe("Testing if the App renders without crashing", () => {
-  it("renders without crashing", () => {
+describe("Testing if App renders without crashing", () => {
+  it("should render without crashing", () => {
     shallow(<App />);
+  });
+});
+
+describe("verifying that App renders some div elements", () => {
+  it("should render a div with the class App-header", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("div.App-header").exists()).toBe(true);
+  });
+
+  it("should render a div with the class App-body", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("div.App-body").exists()).toBe(true);
+  });
+
+  it("should render a div with the class App-footer", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find("div.App-footer").exists()).toBe(true);
   });
 });
