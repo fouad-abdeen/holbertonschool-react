f16e18482572a5e2bb735ceded0c570724f6061f
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-26 21:47:17 +0200

    Update NotificationItem.js

--- 0x03-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -4,20 +4,16 @@ import "./NotificationItem.css";
 export default function NotificationItem(props) {
   if (props.html !== undefined)
     return (
-      <>
         <li
           id={props.type}
           data-notification-type={props.type}
           dangerouslySetInnerHTML={props.html}
         />
-      </>
     );
   else
     return (
-      <>
         <li id={props.type} data-notification-type={props.type}>
           {props.value}
         </li>
-      </>
     );
 }
