c9ed6fbb812422ae723ea952cecc784c868d48b0
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-26 03:10:29 +0200

    Update App.js

--- 0x03-react_props/task_4/dashboard/src/App/App.js
+++ 0x03-react_props/task_4/dashboard/src/App/App.js
@@ -6,7 +6,6 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Login from "../Login/Login";
 import CourseList from "../CourseList/CourseList";
-import CourseListRow from "../CourseList/CourseListRow";
 
 function App(props) {
   let bodyContent;
