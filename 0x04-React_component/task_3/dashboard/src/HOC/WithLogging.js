efb7ba9b081260609763855155526cde25260df5
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-03-02 18:52:42 +0200

    Complete task-7

---
+++ 0x04-React_component/task_3/dashboard/src/HOC/WithLogging.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const WithLogging = (WrappedComponent) => {
+  class WithLogging extends React.Component {
+    componentDidMount() {
+      console.log(`Component ${getDisplayName(WrappedComponent)} is mounted`);
+    }
+    componentWillUnmount() {
+      console.log(`Component ${getDisplayName(WrappedComponent)} is unmounted`);
+    }
+    render() {
+      return <WrappedComponent />;
+    }
+  }
+
+  WithLogging.displayName = `WithLogging(${getDisplayName(WrappedComponent)})`;
+  return WithLogging;
+};
+
+const getDisplayName = (WrappedComponent) => {
+  return WrappedComponent.displayName || WrappedComponent.name || "Component";
+};
+
+export default WithLogging;
