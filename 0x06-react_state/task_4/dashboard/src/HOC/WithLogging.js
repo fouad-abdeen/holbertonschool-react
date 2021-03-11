0f953d62bae5f1109fa0e2309b61087684535b91
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-03-11 23:26:36 +0200

    Complete task-4

---
+++ 0x06-react_state/task_4/dashboard/src/HOC/WithLogging.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const WithLogging = (WrappedComponent) => {
+  class WithLogging extends React.Component {
+    componentDidMount() {
+      console.log(`Component ${getDisplayName(WrappedComponent)} is mounted`);
+    }
+
+    componentWillUnmount() {
+      console.log(
+        `Component ${getDisplayName(WrappedComponent)} is going to unmount`
+      );
+    }
+
+    render() {
+      return <WrappedComponent />;
+    }
+  }
+
+  WithLogging.displayName = `WithLogging(${getDisplayName(WrappedComponent)})`;
+  return WithLogging;
+};
+
+const getDisplayName = (WrappedComponent) => {
+  return WrappedComponent.displayName || WrappedComponent.name || "Component";
+};
+
+export default WithLogging;
---
+++ 0x06-react_state/task_4/dashboard/src/Login/Login.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      enableSubmit: false,
+      email: "",
+      password: "",
+    };
+  }
+
+  handleLoginSubmit = (e) => {
+    this.setState({ isLoggedIn: true });
+    e.preventDefault();
+  };
+
+  handleChangeEmail = (e) => {
+    this.setState({ email: e.target.value });
+
+    if (e.target.value.length < 1) {
+      this.setState({ enableSubmit: false });
+    } else {
+      if (this.state.password.length < 1) {
+        this.setState({ enableSubmit: false });
+      } else {
+        this.setState({ enableSubmit: true });
+      }
+    }
+  };
+
+  handleChangePassword = (e) => {
+    this.setState({ password: e.target.value });
+
+    if (e.target.value.length < 1) {
+      this.setState({ enableSubmit: false });
+    } else {
+      if (this.state.email.length < 1) {
+        this.setState({ enableSubmit: false });
+      } else {
+        this.setState({ enableSubmit: true });
+      }
+    }
+  };
+
+  selectEmail = () => {
+    const myEmail = document.querySelector("#email");
+    myEmail.select();
+  };
+
+  selectPassword = () => {
+    const myPassword = document.querySelector("#password");
+    myPassword.select();
+  };
+
+  render() {
+    return (
+      <>
+        <p>Login to access the full dashboard</p>
+        <form onSubmit={this.handleLoginSubmit}>
+          <label
+            className={css(styles.label)}
+            htmlFor="email"
+            onClick={this.selectEmail}
+          >
+            Email:
+          </label>
+          <input
+            className={css(styles.input)}
+            type="text"
+            name="email"
+            id="email"
+            value={this.state.email}
+            onChange={this.handleChangeEmail}
+          />
+          <br />
+          <label
+            className={css(styles.label)}
+            htmlFor="password"
+            onClick={this.selectPassword}
+          >
+            Password:
+          </label>
+          <input
+            className={css(styles.input)}
+            type="password"
+            name="password"
+            id="password"
+            value={this.state.password}
+            onChange={this.handleChangePassword}
+          />
+          <br />
+          <input
+            disabled={!this.state.enableSubmit}
+            type="submit"
+            value="OK"
+            className={css(styles.input)}
+          />
+        </form>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  label: {
+    cursor: "pointer",
+    margin: "0.125rem",
+  },
+
+  input: {
+    margin: "0.125rem",
+    border: "none",
+    backgroundColor: "transparent",
+  },
+});
+
+export default Login;
