cf0ec356850843ac2b77cc65d074abd72dbcaa7f
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-03-17 01:53:17 +0200

    Complete task-5

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,5 +1,5 @@
-import { Map } from 'immutable';
+import { List, Map } from 'immutable';
 
-export const concatElements = (page1, page2) => page1.concat(page2);
+export const concatElements = (page1, page2) => List(page1.concat(page2));
 
 export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
